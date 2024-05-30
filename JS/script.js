95aa81bbe6e31b3fd01268a4baaffab9d5d31355
azadehazim <azadehazt@gmail.com>
2024-05-30 20:30:49 +0430

    functions for trash fadeOut added

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ let count=0;
 document.querySelector('#input').addEventListener('change',function(){
     count++;
     let input = this.value;
-    let li=`<li id=li${count}>` + input + `<i class="fas fa-check" data-id=${count}></i> <i class="fas fa-trash"></i> </li>`;
+    let li=`<li id=li${count}>` + input + `<i class="fas fa-check" data-id=${count}></i> <i class="fas fa-trash" data-id=${count}></i> </li>`;
     this.value="";
     document.querySelector("ul").innerHTML+=li;
     
@@ -18,16 +18,22 @@ document.addEventListener("click",checkHandler);
 
 function checkHandler(event){
     //console.log(event.target.className);
+    let selectedCheckID=event.target.dataset.id;
+    let selectedLi=document.getElementById("li"+selectedCheckID);
+
     if(event.target.className==="fas fa-check"){
-        //console.log("hi");
-        let selectedCheckID=event.target.dataset.id;
-        //console.log(selectedLi);
-        let selectedLi=document.getElementById("li"+selectedCheckID);
-        //console.log(selectedLi);
-        selectedLi.classList.add("checked")
+        
+        selectedLi.classList.add("checked");
         
     }
+
+
     else if(event.target.className==="fas fa-trash"){
         console.log("bye");
+
+        selectedLi.classList.add("trashed");
+        selectedLi.addEventListener('transitionend', () => selectedLi.remove());
+
     }
+
 }
